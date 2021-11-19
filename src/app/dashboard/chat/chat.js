6b891e3dcce88c851f07ff594e438cc63bed36cb
Abdulhafeez Hameed <laxhameedhafeez@gmail.com>
2021-11-19 10:16:34 +0100

    committing on a new branch

--- src/app/dashboard/chat/chat.js
+++ src/app/dashboard/chat/chat.js
@@ -1,17 +1,13 @@
-import './chat.scss';
+import "./chat.scss";
 
 const Chat = () => {
-    return(
-        <div className="chat-container">
-            Chat Module
-        </div>
-    )
-}
+  return <div className="chat-container">Chat Modules</div>;
+};
 
 export default {
-    routeProps: {
-        path: '/dashboard/chat',
-        component: Chat,
-    },
-    name: 'Chat',
-}
+  routeProps: {
+    path: "/dashboard/chat",
+    component: Chat,
+  },
+  name: "Chat",
+};
