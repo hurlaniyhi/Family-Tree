df7cbedfc65b2da5c8ad9474809a6c2df4ce87c1
AppdevMACBook10 <appdevmacbook10@AppdevMACBook10s-MacBook-Pro.local>
2021-12-03 22:13:11 +0100

    make it better

--- src/app/signin/signin.js
+++ src/app/signin/signin.js
@@ -1,19 +1,35 @@
+import React,{useRef, useState, useContext} from 'react'
 import {useHistory} from 'react-router-dom'
+import AuthContext from '../../providers/state-managers/authenticationManager'
 import './signin.scss';
 
 const SignIn = () => {
     const history = useHistory()
+    const {updateWithLoginData} = useContext(AuthContext)
+    const [data, setData] = useState({username: '', email: ''})
 
     const handleLogin = (route) => {
         history.push(route)
     }
 
+    const handleInput = (e) => {
+        setData({...data, [e.target.name]: e.target.value})
+    }
+
+    function handleSubmit(){
+        updateWithLoginData(data)
+    }
+   
     return(
         <div className="signin-container">
             SignIn Module
             <p className="text">Click <a className="nav" onClick={() => handleLogin('/dashboard')}>here</a> to get to dashboard home page</p>
-            <p className="text">Click <a className="nav" onClick={() => handleLogin('/dashboard/chat')}>here</a> to get to chat page of the dashboard</p>
-            <p className="text">Click <a className="nav" onClick={() => handleLogin('/signup')}>here</a> to get to signup page</p>
+            {/* <p className="text">Click <a className="nav" onClick={() => handleLogin('/dashboard/chat')}>here</a> to get to chat page of the dashboard</p>
+            <p className="text">Click <a className="nav" onClick={() => handleLogin('/signup')}>here</a> to get to signup page</p> */}
+
+            <input placeholder="Enter username" name="username" onChange={handleInput} />
+            <input placeholder="Enter email" name="email" onChange={handleInput} />
+            <button onClick={handleSubmit}>Login</button>
         </div>
     )
 }
@@ -25,3 +41,5 @@ export default {
     },
     name: 'SignIn',
 }
+
+//const memoizedComponent = React.memo(SignIn)
