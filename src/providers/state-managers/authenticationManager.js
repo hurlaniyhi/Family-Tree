c098a7f931157a12c76c0b6163fa00f3d36874f9
Abdulhafeez Hameed <laxhameedhafeez@gmail.com>
2021-12-03 23:00:52 +0100

    testing merge

--- src/providers/state-managers/authenticationManager.js
+++ src/providers/state-managers/authenticationManager.js
@@ -1,36 +1,43 @@
-import React, {useReducer} from 'react'
+import React, { useReducer } from "react";
 
-const AuthContext = React.createContext()
+const AuthContext = React.createContext();
 
 const AuthReducer = (state, action) => {
-    switch(action.type){
-        case "all_data_to_empty":
-            return {...state, username: "", email: ""}
+  switch (action.type) {
+    case "all_data_to_empty":
+      return { ...state, username: "", email: "" };
 
-        case "update_data":
-            console.log({myData: action.payload})
-            return {...state, username: action.payload.username, email: action.payload.email}
-    }
-}
+    case "update_data":
+      console.log({ myData: action.payload });
+      return {
+        ...state,
+        username: action.payload.username,
+        email: action.payload.email,
+      };
+  }
+};
+
+// testing one two
 
 export const AuthProvider = (props) => {
-    const [state, dispatch] = useReducer(AuthReducer, {
-        username: '', email: ''
-    })
+  const [state, dispatch] = useReducer(AuthReducer, {
+    username: "",
+    email: "",
+  });
 
-    async function updateWithLoginData(data){
-        await dispatch({type: "update_data", payload: data})
-    }
+  async function updateWithLoginData(data) {
+    await dispatch({ type: "update_data", payload: data });
+  }
 
-    const boundActions = {
-        updateWithLoginData
-    }
+  const boundActions = {
+    updateWithLoginData,
+  };
 
-    return (
-        <AuthContext.Provider value={{state, ...boundActions}} >
-            {props.children}
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider value={{ state, ...boundActions }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
-export default AuthContext
+export default AuthContext;
